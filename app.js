471eb69d4f8acf57a6a6331ee6dba8f12834c96e
wisang <wseom7@gmail.com>
2022-09-15 17:01:10 +0900

    mongoose introduced

--- app.js
+++ app.js
@@ -1,37 +1,23 @@
-const MongoClient = require("mongodb").MongoClient;
-// Replace the uri string with your connection string.
-
-const assert = require("assert");
 
 // const uri =
 //   "mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&w=majority";
 
-const url = "mongodb://localhost:27017";
+const mongoose = require('mongoose');
 
-const dbName = 'fruitsDB';
+mongoose.connect("mongodb://localhost:27017/fruitsDB");
 
-const client = new MongoClient(url);
+const fruitSchema = new mongoose.Schema({
+  name: String,
+  rating: Number,
+  review: String
+});
 
-client.connect(function(err) {
-  assert.equal(null, err);
-  console.log("connected successfully");
+const Fruit = mongoose.model("Fruit", fruitSchema);
 
-  const db = client.db(dbName);
+const fruit = new Fruit({
+  name: "Apple",
+  rating: 7,
+  review: "very good"
+});
 
-  client.close();
-})
-
-// async function run() {
-//   try {
-//     const database = client.db('sample_mflix');
-//     const movies = database.collection('movies');
-//     // Query for a movie that has the title 'Back to the Future'
-//     const query = { title: 'Back to the Future' };
-//     const movie = await movies.findOne(query);
-//     console.log(movie);
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
+fruit.save();
