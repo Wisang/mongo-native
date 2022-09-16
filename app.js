36e9a497a10caf416f2f76f0ab11dfd66c1bbc96
wisang <wseom7@gmail.com>
2022-09-16 10:08:12 +0900

    created collections with mongoose

--- app.js
+++ app.js
@@ -20,4 +20,36 @@ const fruit = new Fruit({
   review: "very good"
 });
 
-fruit.save();
+const personSchema = new mongoose.Schema({
+  name: String,
+  age: Number
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+  name: "John",
+  age: 37
+});
+
+const kiwi = new Fruit({
+  name: "Kiwi",
+  rating: 8,
+  review: "very good"
+});
+
+const banana = new Fruit({
+  name: "Banana",
+  rating: 9,
+  review: "very good"
+});
+
+Fruit.insertMany([kiwi, banana], function(error) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Success");
+  }
+})
+
+// fruit.save();
